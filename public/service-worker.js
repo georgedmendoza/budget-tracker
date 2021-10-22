ee649d46a5a74ea2103dedb9d8fa20776a76ef17
georgedmendoza <georgedmendoza13@gmail.com>
2021-10-22 16:28:02 -0500

    changes to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,17 @@
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+
+  "/",
+  "./index.html",
+  "./css/style.css",
+  "./js/idb.js",
+  "./js/index.js",
+  "/manifest.js"
+];
+
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
@@ -16,3 +30,34 @@ self.addEventListener('fetch', function (e) {
       })
     )
 })
+
+// self referes to service worker object
+// this instantiates run before the window object has been created
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});

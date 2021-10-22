010dc107099e5f23204e37d513fd47eedd346144
georgedmendoza <georgedmendoza13@gmail.com>
2021-10-22 15:58:02 -0500

    idb.js mostly complete

--- public/js/idb.js
+++ public/js/idb.js
@@ -18,7 +18,7 @@ request.onsuccess = function(event) {
 
     // check if app is online, if yes run function
     if(navigator.onLine) {
-        // uploadFund();
+        uploadFund();
     }
 };
 
@@ -28,7 +28,7 @@ request.onerror = function(event) {
 };
 
 // executed when submit is attempted, but no iternet connection
-function saveFund(fund) {
+function saveRecord(fund) {
     // open new transaction w/ db w/ read and write permession
     const transaction = db.transaction(['new_fund', 'readwrite']);
     // access obj store for new_fund
@@ -36,3 +36,45 @@ function saveFund(fund) {
     // add record to my store with add method
     fundObjectStore.add(fund);
 }
+
+function uploadFund() {
+    // open transaction on db
+    const transaction = db.transaction(['new_fund'], 'readwrite');
+    // access object sotre
+    const fundObjectStore = transaction.objectStore('new_fund');
+    // get all records from store and set to a variable
+    const getAll = fundObjectStore.getAll();
+
+    // if ther is data in indexedDB then send to api server
+    if (getAll.result.length > 0) {
+        fetch("/api/transaction", {
+            method: "POST",
+            body: JSON.stringify(transaction),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(serverResponse => {
+            if (serverResponse.message) {
+                throw new Error(serverResponse);
+            }
+            // open one more transaction
+            const transaction = db.transaction(['new_fund'], 'readwrite');
+            // access new_fund obj store
+            const fundObjectStore = transaction.objectStore('new_fund');
+            // clear all items in store
+            fundObjectStore.clear();
+
+            alert('All funds saved and submitted!')
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+}
+
+
+// listen for app coming back online
+window.addEventListener('online', uploadFund);
